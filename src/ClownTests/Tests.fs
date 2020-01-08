913c8fc857595a2073180d96e0afc0dd9e4a1ebf
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2020-01-08 13:43:09 +0100

    added fscheck@

--- src/ClownTests/Tests.fs
+++ src/ClownTests/Tests.fs
@@ -2,12 +2,14 @@ module Tests
 
 open System
 open Xunit
+open FsCheck.Xunit
 open ClownLib 
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
 
-[<Fact>]
-let ``My test Lib`` () =
-    Assert.Equal(3, ClownLib.Say.yo "22")
+[<Property>]
+let ``teit`` (foo: int) =
+    foo < 10 
+
