c16ba281cce8bf63f326d8b740cb90506bfa919a
Even Brenden <evenbrenden@gmail.com>
2020-01-08 16:24:10 +0100

    Works

--- src/ClownLib/Library.fs
+++ src/ClownLib/Library.fs
@@ -1,9 +1,37 @@
 ﻿namespace ClownLib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Mastermind =
 
-    let yo foo =
-        printfn "Hello %s" foo 
-        2
+    type Color =
+        | Red
+        | Blue
+        | Green
+        | Pink
+        | Yellow
+        | Turkis
+
+    let remove (e: Color) (l: Color List) =
+        let rec acc e l res =
+            match l with
+            | x :: xs -> if x = e then res @ xs else acc e xs res
+            | [] -> res
+        acc e l []
+
+    let diff (answer:Color list) (guess:Color list) : int * int =
+        let rightColorRightPosition =
+            List.indexed guess
+            |> List.filter (fun (i, c) -> answer.[i] = c)
+            |> List.map (fun (i, c) -> c)
+        let remaining =
+            List.fold
+                (fun s c -> remove c s)
+                answer
+                rightColorRightPosition
+        let rightColorOnly =
+            guess
+            |> List.fold (fun (result, remaining) c ->
+                if List.contains c remaining
+                    then (c :: result, remove c remaining)
+                    else (result, remaining))
+                ([], remaining)
+        (List.length rightColorRightPosition, List.length (fst rightColorOnly))
--- src/ClownTests/Tests.fs
+++ src/ClownTests/Tests.fs
@@ -1,15 +1,72 @@
 module Tests
 
-open System
 open Xunit
 open FsCheck.Xunit
-open ClownLib 
+open ClownLib.Mastermind
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``B R -> (0, 0)``() =
+    Assert.Equal((0, 0), diff [Blue] [Red])
+
+
+[<Fact>]
+let ``B B -> (1, 0)``() =
+    Assert.Equal((1, 0), diff [Blue] [Blue])
+
+[<Fact>]
+let ``RY BR -> (0, 1) ``() =
+    Assert.Equal((0, 1), diff [Red; Yellow] [Blue; Red])
+
+[<Fact>]
+let ``GBRP YYGG -> (0, 1)``() =
+    Assert.Equal((0, 1), diff [Green; Blue; Red; Pink] [Yellow; Yellow; Green; Green])
+
+[<Fact>]
+let ``GBRP PRRY -> (1, 1)``() =
+    Assert.Equal((1, 1), diff [Green; Blue; Red; Pink] [Pink; Red; Red; Yellow])
+
+[<Fact>]
+let ``GBRP GPGY -> (1, 1)``() =
+    Assert.Equal((1, 1), diff [Green; Blue; Red; Pink] [Green; Pink; Green; Yellow])
+
+[<Fact>]
+let ``GBRP RRPY -> (1, 1)``() =
+    Assert.Equal((0, 2), diff [Green; Blue; Red; Pink] [Red; Red; Pink; Yellow])
+
+type FourColors = {a: Color; b:Color; c: Color; d:Color}
 
 [<Property>]
-let ``teit`` (foo: int) =
-    foo < 10 
+let ``can not be more right than four`` (guess: FourColors) (answer: FourColors) =
+    let guess = [guess.a; guess.b; guess.c; guess.d]
+    let answer = [answer.a; answer.b; answer.c; answer.d]
+    let (foo, bar) = diff answer guess
+    let sum = foo + bar
+    Assert.True (sum <= 4 && sum >= 0)
 
+[<Property>]
+let ``unicolors are easy`` (guess: Color) (answer: Color) =
+    let guess = List.replicate 4 guess
+    let answer = List.replicate 4 answer
+    let (foo, bar) = diff answer guess
+    if (guess = answer) then
+        Assert.True(foo = 4 && bar = 0)
+    else
+        Assert.True(foo=0 && bar = 0)
+
+[<Property>]
+let ``correct answer are easy``  (guess: FourColors) =
+    let guess' = [guess.a; guess.b; guess.c; guess.d]
+
+    let (foo, bar) = diff guess' guess'
+    Assert.True(foo = 4 && bar = 0)
+
+[<Property>]
+let ``rett er rett og galt er galt`` (guess: FourColors) (answer: FourColors) =
+    let guess = [guess.a; guess.b; guess.c; guess.d]
+    let answer = [answer.a; answer.b; answer.c; answer.d]
+    let (foo, bar) = diff guess guess
+    if (guess = answer) then
+        Assert.True(foo = 4 && bar = 0)
+    else
+        Assert.True(bar < 4)
